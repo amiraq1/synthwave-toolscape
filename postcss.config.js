41e493f8610b8fa62d0cb90d30a6e3dd741413f1
Amiraq <amaralmdarking27@gmail.com>
2025-12-29 17:47:07 +0300

    build: Enable CSS minification for production builds using cssnano.

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,7 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // CSS minification in production (cssnano is bundled with Vite)
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
 };
