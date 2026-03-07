40a02306c2d1d18f2ee2f454c195b15e31137b58
amiraq1 <amaralmdarking27@gmail.com>
2026-03-07 22:26:01 +0300

    Fix Supabase runtime fallback and request rewrite

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,5 +3,6 @@ export * from './imageOptimizer';
 export * from './imageUrl';
 export * from './localization';
 export * from './performance';
+export * from './runtimeConfig';
 export * from './supabaseUrl';
 export * from './toolUrl';
